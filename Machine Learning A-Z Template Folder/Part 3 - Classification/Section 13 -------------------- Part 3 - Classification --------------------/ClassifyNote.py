f9f1eb8801879a57f77bb65f037c9fbe0c032bc6
Charlotte <xiangyinyu1127@gmail.com>
2019-03-15 14:09:18 -0400

    aaa

--- Machine Learning A-Z Template Folder/Part 3 - Classification/Section 13 -------------------- Part 3 - Classification --------------------/ClassifyNote.py
+++ Machine Learning A-Z Template Folder/Part 3 - Classification/Section 13 -------------------- Part 3 - Classification --------------------/ClassifyNote.py
@@ -72,15 +72,89 @@ and we have a new point p, how to we know it belongs to c1 or c2?
 
 Step 1. Choose the number K of neighbors.
 Step 2. Take the K nearest neighbors of the new data point, 
-        accoding to the Euclidean dstance
+        accoding to the Euclidean distance
 Step 3. Among these K neighbors, count the number of data points in each category
 Step 4. Assign the new data point to the category 
         where you counted the most neighbors.
 
+Notice: K is the number of neighbors you choose, Not A Radius!!
+'''
+
+
+'''---------------------------------Section 16-------------------------------'''
+# SVM
+'''It's to find the best fitting boundary which helps us to separate different classes.
+
+Say we have two classes data with their margin points.
+And we use these margin points to find two parallels that forms a gap between twwo classes.
+- And this gap is the Maximum Margin, and these points we call Suppport Vectors.
+
+- The line in the middle of this gap is called Maximum Margin Hyperplane.
+(or Maximum Margin Classfier)
+
+- The two margin lines are called Positive Hyperplane and Negative Hyperplane, respectively.
+'''
+
+# What's so special about SVM?
 
 
 
+'''---------------------------------Section 16-------------------------------'''
+# Kernel SVM
+'''
+In the last case, we have panel of points that can be seperated with a linear line easily.
+Then what about other situations like,
+some points are surrounded by other points, (Non linear models)?
 
+So SVM is to use these support vectors to find the proper boundary (Assumption)
+'''
+
+
+# Higher-Dimension Space
+'''
+For example, in one-dimension space, we have:
+    ---------- ○ ○ ○ ○ ● ● ● ○ ○ ○ ----->
+    
+It's not linear in the line, and we want to separate them linearly.
+We can made some computaions with these variables like f = (x-5)^2
+Then these dots are like balls, bounce on to 2-dimensional plane.
+And then we can separate them with a line.
+
+Same thing to 2-D space:
+    F1(x1,x2) ==============>> F2(x1,x2,z)
+              Mapping function
+
+z is the new dimension, and we can separate points using hyperplane.
+
+After that, we need to project them back into 2-D:
+    F2(x1,x2,z) ==============>> F1(x1,x2)
+                  Projection
+In 2-D, It still looks like non-linear separator.
+'''
+
+# Mapping to higher dimension can be really Compute-Intensive!
+# Kernel Trick doesn't require mapping.
+
+# Kernel Rules
+''' K(-) = 1   if condition
+           0   otherwise
+'''
+
+
+# The Gaussian RBF kernel
+'''
+    K(x,li) = e^(-|x-li|^2 / 2σ^2)
+                     ^
+                  Distance^2
+
+σ↑ --> width↑
+'''
+
+# Type of Kernel Functions
+'''
+Gaussian
+Sigmoid: K(X,Y) = tanh(v.X`Y+r) ---> 'S' model
+Polynomial
 
 
 

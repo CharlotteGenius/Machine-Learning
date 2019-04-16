310ae1035aaf7df0b6aed51e3a1231086b0245da
Charlotte <xiangyinyu1127@gmail.com>
2019-04-16 16:51:18 -0400

    ..

--- Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 31 -------------------- Part 6 - Reinforcement Learning --------------------/RLnote.py
+++ Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 31 -------------------- Part 6 - Reinforcement Learning --------------------/RLnote.py
@@ -16,7 +16,7 @@ E.g. After 10 rounds, according to 10 rounds result, we'll decide which ads it'l
 It depends on the observations at the beginning of the experiment up to the present time.
 '''
 
-'''----------------------------- Section 27 --------------------------------'''
+'''----------------------------- Section 32 --------------------------------'''
 # Upper Confidence Bound (UCB)
 
 # the Multi-armed Bandit Problem
@@ -47,13 +47,46 @@ Step 2. From these two numbers we compute:
         δi(n) = sqrt{ 3log(n) / 2Ni(n) }
         
 Step 3. Select ad i that has maximum UCB ri(n)+δi(n)
+
+先对每一个臂都试一遍
+之后，每次选择UCB值最大的那个臂
+其中ri是这个臂到目前的收益均值，δi叫做bonus
+本质上是均值的标准差，n是目前的试验次数
+
+这个公式反映：均值越大，标准差越小，被选中的概率会越来越大，起到了exploit的作用；
+同时哪些被选次数较少的臂也会得到试验机会，起到了explore的作用。
+
 '''
 
 
-'''----------------------------- Section 28 --------------------------------'''
+'''----------------------------- Section 33 --------------------------------'''
 # Thompson Sampling
 # a probabilistic algorithm
 
+'''
+Step1. At each round n, we consider two numbers for each ad i:
+    - Ni1(n) the # of times the ad i got reward 1 up to round n,
+    - Ni0(n) the # of times the ad i got reward 0 up to round n.
+
+Step 2. For each ad i, we take a random draw from the distribution  below:
+    θi(n) = β( Ni1(n)+1, Ni0(n)+1 )
+
+Step 3. We select the ad that has the highest θi(n)
+'''
+
+'''
+https://zhuanlan.zhihu.com/p/21388070
+
+假设每个臂是否产生收益，其背后有一个概率分布，产生收益的概率为p
+我们不断地试验，去估计出一个置信度较高的*概率p的概率分布*就能近似解决这个问题了。
+怎么能估计概率p的概率分布呢？ 
+答案是假设概率p的概率分布符合beta(win, lose)分布，它有两个参数: win, lose。
+每个臂都维护一个beta分布的参数。每次试验后，选中一个臂，摇一下，有收益则该臂的win增加1，否则该臂的lose增加1。
+每次选择臂的方式是：用每个臂现有的beta分布产生一个随机数b，选择所有臂产生的随机数中最大的那个臂去摇。
+'''
+
+# code
+choice = numpy.argmax(pymc.rbeta(1 + self.wins, 1 + self.trials - self.wins))
 
 
 
--- Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/Thompson_Sampling/thompson_sampling.py
+++ Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/Thompson_Sampling/thompson_sampling.py
@@ -13,24 +13,37 @@ import random
 N = 10000
 d = 10
 ads_selected = []
-numbers_of_rewards_1 = [0] * d
-numbers_of_rewards_0 = [0] * d
+
+'''Step1. At each round n, we consider two numbers for each ad i:
+    - Ni1(n) the # of times the ad i got reward 1 up to round n,
+    - Ni0(n) the # of times the ad i got reward 0 up to round n.
+'''
+numbers_of_rewards_1 = [0] * d # Ni1(n)
+numbers_of_rewards_0 = [0] * d # Ni0(n)
+
 total_reward = 0
 for n in range(0, N):
     ad = 0
     max_random = 0
     for i in range(0, d):
         random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        '''Step 2. For each ad i, we take a random draw from the distribution:
+            θi(n) = β( Ni1(n)+1, Ni0(n)+1 )'''
         if random_beta > max_random:
             max_random = random_beta
+            '''Step 3. We select the ad that has the highest θi(n)'''
             ad = i
     ads_selected.append(ad)
+    
+    # update two numbers
     reward = dataset.values[n, ad]
     if reward == 1:
         numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
     else:
         numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    
     total_reward = total_reward + reward
+
 
 # Visualising the results - Histogram
 plt.hist(ads_selected)

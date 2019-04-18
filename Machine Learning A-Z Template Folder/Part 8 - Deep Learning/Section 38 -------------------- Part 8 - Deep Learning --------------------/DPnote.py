cc6f9da8ccea76dcc461f7eed7fcc2693c3b6c10
Charlotte <xiangyinyu1127@gmail.com>
2019-04-18 11:43:26 -0400

    。。

--- Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 38 -------------------- Part 8 - Deep Learning --------------------/DPnote.py
+++ Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 38 -------------------- Part 8 - Deep Learning --------------------/DPnote.py
@@ -8,3 +8,72 @@ Created on Wed Apr 17 13:20:55 2019
 
 # ================================= Part 8 ===================================
 # Deep Learning
+'''
+Deep Learning is the most exciting and powerful branch of Machine Learning. Deep Learning models can be used for a variety of complex tasks:
+
+    - Artificial Neural Networks for Regression and Classification
+    - Convolutional Neural Networks for Computer Vision
+    - Recurrent Neural Networks for Time Series Analysis
+    - Self Organizing Maps for Feature Extraction
+    - Deep Boltzmann Machines for Recommendation Systems
+    - Auto Encoders for Recommendation Systems
+
+In this part, you will understand and learn how to implement the following Deep Learning models:
+    - Artificial Neural Networks for a Business Problem
+    - Convolutional Neural Networks for a Computer Vision task
+'''
+
+# Geoffrey Hinton: Father of Deep Learning
+
+'''----------------------------- Section 39 --------------------------------'''
+# In this Section we'll learn:
+    # The Nueron
+    # The Activation Function
+    # How do Nueral Networkd work?
+    # How do Nueral Networks learn?
+    # Gradient Descent
+    # Stochastic Gradient Descent
+    # Backpropagation
+
+# The Nueron
+    dendrite; axon
+    
+# The Activation Function
+    1. Threshold Function:
+        1 for x>0; 0 for x<0 (yes or no)
+    2. Sigmoid Function:
+        1/(1+e^(-x))
+    3. Rectifier:
+        max(x,0)
+    4. Hyperbolic Tangent (tanh):
+        (1-e^(-2x))/(1+e^(-2x))
+
+
+# How do Nueral Networkd work?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

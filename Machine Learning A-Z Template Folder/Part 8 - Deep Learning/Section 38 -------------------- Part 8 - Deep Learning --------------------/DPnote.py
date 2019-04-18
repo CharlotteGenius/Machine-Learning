603c0aea43582f5b7ab598afece0789ad004fddd
Charlotte <xiangyinyu1127@gmail.com>
2019-04-18 19:57:22 -0400

    ..

--- Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 38 -------------------- Part 8 - Deep Learning --------------------/DPnote.py
+++ Machine Learning A-Z Template Folder/Part 8 - Deep Learning/Section 38 -------------------- Part 8 - Deep Learning --------------------/DPnote.py
@@ -51,8 +51,27 @@ In this part, you will understand and learn how to implement the following Deep 
 
 # How do Nueral Networkd work?
 
+Backpropagation:
+        Cost Function implies difference between y & ŷ (Actuall & Predicted)
+        An example of cost function: C = ∑ (1/2) * (ŷ - y)²
+        
+        With a series of weights we get a cost function and then go back to adjust the weights and run again, to minimize the cost function.
 
+# Gradient Descent 梯度下降法/最速下降法
+"Gradient descent is an optimization algorithm used to minimize some function by iteratively moving in the direction of steepest descent as defined by the negative of the gradient. In machine learning, we use gradient descent to update the parameters of our model."
+" 要使用梯度下降法找到一个函数的局部极小值，必须向函数上当前点对应梯度的反方向的规定步长距离点进行迭代搜索。如果相反地向梯度正方向迭代进行搜索，则会接近函数的局部极大值点；这个过程则被称为梯度上升法。"
 
+# Curse of Dimensity
+
+FLOPS: floating operation per second
+Sunway Taihulight is the fastest computer in the world and can work 93 PFLOPS
+
+if the function is not convex, then we might find a local minimum cost function.:
+
+# Batch Gradient Descent
+Adjust w after all rows
+# Stochastic Gradient Descent
+Adjust w every time we run a row of data
 
 
 
